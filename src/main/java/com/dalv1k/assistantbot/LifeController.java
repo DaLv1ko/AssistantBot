030125b554fd37255211462124a8403389e508f4
DaLv1ko <vlad1602dubno2552dalv@gmail.com>
2021-08-01 03:54:30 +0300

    heroku fix

---
+++ src/main/java/com/dalv1k/assistantbot/LifeController.java
@@ -0,0 +1,12 @@
+package com.dalv1k.assistantbot;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LifeController {
+
+    @GetMapping
+    public void live(){
+    }
+}
